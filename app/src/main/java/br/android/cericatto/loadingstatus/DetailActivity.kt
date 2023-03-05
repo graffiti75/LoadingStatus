d0c8ab997bfa6c335f49f303cf382ee645292996
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-03-05 17:23:37 -0500

    Detail screen layout updated

--- app/src/main/java/br/android/cericatto/loadingstatus/DetailActivity.kt
+++ app/src/main/java/br/android/cericatto/loadingstatus/DetailActivity.kt
@@ -2,11 +2,20 @@ package br.android.cericatto.loadingstatus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        initButtonListeners()
+    }
+
+    private fun initButtonListeners() {
+        okButton.setOnClickListener {
+            finish()
+        }
     }
 }
